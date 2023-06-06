60a006d1b49a4cc939c272ace218b448f612fdea
haeunNoh06 <okhagrace06@naver.com>
2023-06-06 14:01:36 +0900

    [study] DOM 객체와 이벤트 리스너 알아보기

---
+++ study/04_DOM/js/DOM_Event.js
@@ -0,0 +1,11 @@
+// var button = document.getElementById("btn1");
+// button.onclick = function() {
+//   console.log("버튼 클릭!");
+// };
+
+function doClick() {
+    console.log("버튼 클릭! ver2.");
+}
+
+var button1 = document.getElementById("btn1");
+button1.onclick = doClick;
